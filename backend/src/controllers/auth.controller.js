32598655b161b356d9cb37ca92636987f1e893f4
fxckcode <diegoale8582@hotmail.com>
2024-08-02 16:40:58 -0500

    prueba

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body
-        const [ result ] = await pool.query('select id, name, email, phone, role from users where email=? and password=?', [email, password])
+        const [ result ] = await pool.query('select id, name, email, phone, role, identification from users where email=? and password=?', [email, password])
         if (result.length > 0) {
             const token = jwt.sign({ result }, 'tokensecreto', { expiresIn: '24h' })
             const user = result[0]
