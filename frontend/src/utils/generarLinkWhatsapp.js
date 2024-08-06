0a12ca12a5dde6b0edb334b5a073ba1982434b25
fxckcode <diegoale8582@hotmail.com>
2024-08-05 22:22:11 -0500

    v4

--- frontend/src/utils/generarLinkWhatsapp.js
+++ frontend/src/utils/generarLinkWhatsapp.js
@@ -2,7 +2,7 @@ export function generarLinkWhatsApp(numero, mensaje) {
     const mensajeFormato = encodeURIComponent(mensaje); // Codifica el mensaje para la URL
 
     // Genera el enlace
-    const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=57${numeroFormato}&text=${mensajeFormato}`;
+    const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=57${numero}&text=${mensajeFormato}`;
     lo
     
     return enlaceWhatsApp;
